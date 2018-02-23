86af2c3d72fa7595584cb4b90b77d5a87f899289
Florin Leon <36703294+florinleon@users.noreply.github.com>
2018-02-23 13:18:34 +0200

    Add files via upload

---
+++ E10 Iterated Prisoner Dilemma/IteratedPrisonerDilemma/ConfessPrisonerAgent.cs
@@ -0,0 +1,25 @@
+﻿/**************************************************************************
+ *                                                                        *
+ *  Website:     https://github.com/florinleon/ActressMas                 *
+ *  Description: Iterated Prisoner's Dilemma using ActressMas framework   *
+ *  Copyright:   (c) 2018, Florin Leon                                    *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation. This program is distributed in the      *
+ *  hope that it will be useful, but WITHOUT ANY WARRANTY; without even   *
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR   *
+ *  PURPOSE. See the GNU General Public License for more details.         *
+ *                                                                        *
+ **************************************************************************/
+
+namespace IteratedPrisonersDilemma
+{
+    public class ConfessPrisonerAgent : PrisonerAgent
+    {
+        protected override string ChooseAction(int lastOutcome)
+        {
+            return "confess";
+        }
+    }
+}
